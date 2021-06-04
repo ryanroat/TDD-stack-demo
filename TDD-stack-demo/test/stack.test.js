ecf4c5e47fa6fee83d1047cf3c31fb9d504c6fbe
Ryan Roat <ryanroat@gmail.com>
2021-06-04 15:40:09 -0400

    add peek and pop methods

--- TDD-stack-demo/test/stack.test.js
+++ TDD-stack-demo/test/stack.test.js
@@ -5,16 +5,46 @@ class Stack {
     this.top = -1;
     this.items = {};
   }
+
+  push(item) {
+    this.top += 1;
+    this.items[this.top] = item;
+  }
+
+  get peek() {
+    return this.items[this.top];
+  }
+
+  get pop() {
+    this.top -= 1;
+    return this.items[this.top + 1];
+  }
 }
 
 describe('No Array Stack', () => {
-  it('is created empty', ()=>{
-    const stack = new Stack();
+  let stack;
 
-    expect(stack.top).toBe(-1);
+  beforeEach(() => {
+    stack = new Stack();
   });
 
-  it.todo('can push to the top');
+  it('is created empty', () => {
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+  });
 
-  it.todo('can pop off');
+  it('can push to the top', () => {
+    stack.push('an item');
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe('an item');
+  });
+
+  it('can pop off', () => {
+    stack.push('an item');
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe('an item');
+    popped = stack.pop;
+    expect(stack.top).toBe(-1);
+    expect(popped).toBe('an item');
+  });
 });
